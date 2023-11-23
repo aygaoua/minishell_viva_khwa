069b615aa768c9bf4238f7a5a93031b01b4f9860
aygaoua <102914808+aygaoua@users.noreply.github.com>
2023-11-23 20:43:07 +0100

    the simpale command with redirection in with no errors is working fine

--- minishell_viva_khawa/parssing3.c
+++ minishell_viva_khawa/parssing3.c
@@ -6,22 +6,72 @@
 /*   By: azgaoua <azgaoua@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 03:48:42 by azgaoua           #+#    #+#             */
-/*   Updated: 2023/11/23 06:13:18 by azgaoua          ###   ########.fr       */
+/*   Updated: 2023/11/23 19:06:50 by azgaoua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void ft_swap_and_null(char ***array, int row)
-{
-    char *s;
 
-    while ((*array)[row + 1])
-    {
-        s = (*array)[row];
+void ft_swap_and_null(char ***array, int row) {
+    printf("***********|||||||||||||*********\n%s - %d\n***********|||||||||||||*********\n", (*array)[row], row);
+    if (*array == NULL || row < 0 || (*array)[row] == NULL) {
+        // (*array)[row] = NULL;
+        // Handle invalid input or if the specified element is already NULL
+        return;
+    }
+
+    char *temp; // Temporary variable for swapping
+
+    // Set the specified element to NULL
+
+    // Continue swapping until the next element after the specified row is NULL
+    while ((*array)[row + 1] != NULL) {
+        // Swap the current element with the next element
+        temp = (*array)[row];
         (*array)[row] = (*array)[row + 1];
-        (*array)[row + 1] = s;
+        (*array)[row + 1] = temp;
+
+        // Move to the next row
         row++;
     }
-    (*array)[row - 1] = NULL;
+    (*array)[row] = NULL;
+    // (*array)[row] = NULL;
 }
+
+
+// void ft_swap_and_null(char ***array, int row) {
+//     if (*array == NULL || row < 0) {
+//         return;
+//     }
+//     char *temp; // Temporary variable for swapping
+
+//     // Continue swapping until the next element after the specified row is NULL
+//     while ((*array)[row + 1] != NULL) {
+//         // Swap the current element with the next element
+//         temp = (*array)[row];
+//         (*array)[row] = (*array)[row + 1];
+//         (*array)[row + 1] = temp;
+
+//         // Move to the next row
+//         row++;
+//     }
+
+//     // Set the last element to NULL
+//     (*array)[row - 1] = NULL;
+// }
+
+
+// void ft_swap_and_null(char ***array, int row)
+// {
+//     char *s;
+
+//     while ((*array)[row + 1])
+//     {
+//         s = (*array)[row];
+//         (*array)[row] = (*array)[row + 1];
+//         (*array)[row + 1] = s;
+//         row++;
+//     }
+//     (*array)[row] = NULL;
+// }
