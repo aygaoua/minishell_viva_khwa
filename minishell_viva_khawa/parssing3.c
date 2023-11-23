4ef02cfa353e5642e32f345ed51a2cc10252c2f9
aygaoua <102914808+aygaoua@users.noreply.github.com>
2023-11-23 06:27:28 +0100

    simple problem in redirections befor the last one hows returned in the struct git add .

--- minishell_viva_khawa/parssing3.c
+++ minishell_viva_khawa/parssing3.c
@@ -6,7 +6,7 @@
 /*   By: azgaoua <azgaoua@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 03:48:42 by azgaoua           #+#    #+#             */
-/*   Updated: 2023/11/23 05:15:18 by azgaoua          ###   ########.fr       */
+/*   Updated: 2023/11/23 06:13:18 by azgaoua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,11 @@ void ft_swap_and_null(char ***array, int row)
 {
     char *s;
 
-    while ((*array)[row])
+    while ((*array)[row + 1])
     {
-        if ((*array)[row + 1])
-        {
-            s = (*array)[row];
-            (*array)[row] = (*array)[row + 1];
-            (*array)[row + 1] = s;
-        }
+        s = (*array)[row];
+        (*array)[row] = (*array)[row + 1];
+        (*array)[row + 1] = s;
         row++;
     }
     (*array)[row - 1] = NULL;
